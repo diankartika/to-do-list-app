03179cc1bcb5a093150b49994ba428ca2306c208
diankartika <76087230+diankartika@users.noreply.github.com>
2024-11-18 13:08:20 +0700

    Delete lib directory

--- lib/index.dart
+++
@@ -1,6 +0,0 @@
-// Export pages
-export '/pages/started/started_widget.dart' show StartedWidget;
-export '/pages/signin/signin_widget.dart' show SigninWidget;
-export '/home/home_widget.dart' show HomeWidget;
-export '/main_to_do/main_to_do_widget.dart' show MainToDoWidget;
-export '/progress/progress_widget.dart' show ProgressWidget;
